746609184426504cda6b22070d053370482f632c
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-20 12:15:35 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.vamsi.id
   cidr_block              = var.subnet1_cidr
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-north-1a"
 
   tags = {
     Name = "public-subnet1"
