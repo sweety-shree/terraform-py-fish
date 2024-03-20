6aedfe31da706a9368a8e679a89288c55d208783
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-20 12:03:59 +0530

    Update data1.sh

--- data1.sh
+++ data1.sh
@@ -1,5 +1,5 @@
 sudo yum install -y git
-git clone https://github.com/k-vamc/fish-py.git
+git clone https://github.com/k-vamc/fish.git
 cd fish-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
