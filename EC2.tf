c2f8812f7c3caaa17f0f1ab1aaa62535f3568439
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-20 12:20:11 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "fish" {
   ami                         = "ami-04175dfed7619fb38"
   instance_type               = "t2.micro"
-  key_name                    = "stockholm.pem"
+  key_name                    = "stockholm"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
